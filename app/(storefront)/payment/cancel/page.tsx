01ae1375306443594ccdc80013522429c0cee32c
NeoMH91 <neomenghwee1991@hotmail.com>
2024-07-18 10:18:09 +0800

    fixed some errors with eslint

--- app/(storefront)/payment/cancel/page.tsx
+++ app/(storefront)/payment/cancel/page.tsx
@@ -16,7 +16,7 @@ export default function CaencelRoute() {
           <div className="mt-3 text-center sm:mt-5 w-full">
             <h3 className="text-lg leading-6 font-medium">Payment Cancelled</h3>
             <p className="mt-2 text-sm text-muted-foreground">
-              Something went wrong, you haven't been charged. Please try again.
+              Something went wrong, you havent been charged. Please try again.
             </p>
             <Button asChild className="w-full mt-5 sm:mt-6">
               <Link href={"/"}>Back to Homepage</Link>
