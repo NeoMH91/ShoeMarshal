c3968238655310b47c6e371a7bb8d146e60ef263
NeoMH91 <neomenghwee1991@hotmail.com>
2024-07-18 10:56:30 +0800

    replaced localhost:3000 with vercel url

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://shoe-marshal-bhtieybqg-neomh91s-projects.vercel.app/"
+      : "https://shoe-marshal-taupe.vercel.app/"
   );
 }
